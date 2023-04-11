466c552ec2425866d13965be87b2070434972093
mateoscarlos <carlospe.mateos@gmail.com>
2023-04-11 21:02:41 +0200

    Wiring up widgets in a fragment

--- app/src/main/java/com/example/criminalintent/CrimeFragment.kt
+++ app/src/main/java/com/example/criminalintent/CrimeFragment.kt
@@ -1,14 +1,20 @@
 package com.example.criminalintent
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import androidx.fragment.app.Fragment
 
 class CrimeFragment : Fragment() {
 
     private lateinit var crime: Crime
+    private lateinit var titleField: EditText
+    private lateinit var dateButton: Button
 
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,11 +23,39 @@ class CrimeFragment : Fragment() {
 
     // Instead of setContentView in Activities, fragments requires overriding onCreateView because
     // you can inflate and hide the view whenever.
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) : View? {
+    public override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) : View? {
 
         // Inflating the view:          Layout ID          View's parent   Attach to parent
         val view = inflater.inflate(R.layout.fragment_crime, container, false) // Fragment's view will be hosted in the activity´s container view
+        titleField = view.findViewById(R.id.crime_title_edit_text) as EditText
+        dateButton = view.findViewById(R.id.crime_date_button) as Button
+
+        dateButton.apply {
+            text = crime.date.toString()
+            isEnabled = false
+        }
+
         return view
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        val titleWatcher = object : TextWatcher {
+
+            override fun beforeTextChanged(sequence: CharSequence?, start: Int, before: Int, after: Int) {
+                // This space intentionally left blank
+            }
+
+            override fun onTextChanged(sequence: CharSequence?, start: Int, before: Int, count: Int) {
+                crime.title = sequence.toString()
+            }
+
+            override fun afterTextChanged(sequence: Editable?) {
+                // This one too
+            }
+        }
+
+        titleField.addTextChangedListener(titleWatcher)
     }
 
 
